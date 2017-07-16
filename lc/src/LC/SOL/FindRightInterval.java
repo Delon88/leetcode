28d7e9bf792b49a9dae3630592348cd7caff8b47
U-DESKTOP-ASVFKQ3\nanhong <Mr.AlainDelon@gmail.com>
2017-07-15 23:32:48 -0700

    i

---
+++ lc/src/LC/SOL/FindRightInterval.java
@@ -0,0 +1,14 @@
+package LC.SOL;
+
+import LC.DS.Interval;
+
+/**
+ * Created by nanhong on 7/15/2017.
+ */
+public class FindRightInterval {
+    public class Solution {
+        public int[] findRightInterval(Interval[] intervals) {
+
+        }
+    }
+}
