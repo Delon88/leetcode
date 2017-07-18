95dc3c38a5e393d4189fe80312317bf03151e426
U-DESKTOP-ASVFKQ3\nanhong <Mr.AlainDelon@gmail.com>
2017-07-17 22:50:55 -0700

    i

--- lc/src/LC/SOL/FindRightInterval.java
+++ lc/src/LC/SOL/FindRightInterval.java
@@ -2,13 +2,31 @@ package LC.SOL;
 
 import LC.DS.Interval;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * Created by nanhong on 7/15/2017.
  */
 public class FindRightInterval {
     public class Solution {
         public int[] findRightInterval(Interval[] intervals) {
+            int len = intervals.length;
+            int[] ret = new int[intervals.length];
+            TreeMap<Integer, Integer> map = new TreeMap<>();
+            for (int i = 0; i < len; i++) {
+                map.put(intervals[i].start, i);
+            }
 
+            for (int i = 0; i < intervals.length; i++) {
+                Map.Entry<Integer, Integer> ceil = map.ceilingEntry(intervals[i].end);
+                if (ceil != null) {
+                    ret[i] = ceil.getValue();
+                } else {
+                    ret[i] = -1;
+                }
+            }
+            return ret;
         }
     }
 }
