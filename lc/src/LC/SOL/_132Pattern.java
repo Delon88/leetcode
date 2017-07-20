4bead04f166cc4b444c27f0be17958d411ad0f88
Nanhong <nanhongy@amazon.com>
2017-07-19 21:57:00 -0700

    x.

---
+++ lc/src/LC/SOL/_132Pattern.java
@@ -0,0 +1,22 @@
+package LC.SOL;
+
+public class _132Pattern {
+    public class Solution {
+//        public boolean find132pattern(int[] nums) {
+//
+//            if (nums.length < 3) return false;
+//            for (int i = 0; i < nums.length - 2; i++) {
+//                for (int j = i + 1; j < nums.length - 1; j++) {
+//                    for (int k = j + 1; k < nums.length; k++) {
+//                        if ( nums[i] < nums[k] && nums[k] < nums[j]) return true;
+//                    }
+//                }
+//            }
+//            return false;
+//        }
+
+        public boolean find132pattern(int[] nums) {
+            
+        }
+    }
+}
