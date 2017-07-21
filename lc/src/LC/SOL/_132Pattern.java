c7f0c3d281d3ddf4c9442bb02d74afe8b1b18ac3
Nanhong Ye <nanhongy@amazon.com>
2017-07-20 17:19:26 -0700

    ii

--- lc/src/LC/SOL/_132Pattern.java
+++ lc/src/LC/SOL/_132Pattern.java
@@ -18,33 +18,32 @@ public class _132Pattern {
 //        }
 
 
-        [3,5,0,3,4]
-        3,5
-        0, 0
-
         class Pair {
             int min, max;
-            public Pair( int min , int max ) {
+
+            public Pair(int min, int max) {
                 this.min = min;
                 this.max = max;
             }
         }
+
         public boolean find132pattern(int[] nums) {
             Stack<Pair> stack = new Stack<>();
-            if ( nums == null || nums.length < 3  ) {
+            if (nums == null || nums.length < 3) {
                 return false;
             }
-            stack.push( new Pair( nums[0] , nums[0]));
+            stack.push(new Pair(nums[0], nums[0]));
 
-            for ( int i = 1; i < nums.length ; i++) {
-                if ( stack.isEmpty() || nums[i] < stack.peek().min ) stack.push(new Pair(nums[i], nums[i]));
-                else if ( nums[i] > stack.peek().min) {
+            for (int i = 1; i < nums.length; i++) {
+                if (stack.isEmpty() || nums[i] < stack.peek().min) stack.push(new Pair(nums[i], nums[i]));
+                else if (nums[i] > stack.peek().min) {
                     Pair p = stack.pop();
-                    if ( nums[i] < p.max) return true;
+                    if (nums[i] < p.max) return true;
                     else {
                         p.max = nums[i];
-                        while ( !stack.isEmpty() && nums[i] >= stack.peek().max) stack.pop();
-
+                        while (!stack.isEmpty() && nums[i] >= stack.peek().max) stack.pop();
+                        if (!stack.isEmpty() && stack.peek().min < nums[i]) return true;
+                        stack.push(p);
                     }
                 }
             }
