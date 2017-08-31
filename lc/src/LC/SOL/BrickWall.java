cdccd3d801a4ffc5e18ac7191bbd61c184205c40
Nanhong Ye <nanhongy@amazon.com>
2017-08-30 19:00:54 -0700

    x.

---
+++ lc/src/LC/SOL/BrickWall.java
@@ -0,0 +1,11 @@
+package LC.SOL;
+
+import java.util.List;
+
+public class BrickWall {
+    class Solution {
+        public int leastBricks(List<List<Integer>> wall) {
+
+        }
+    }
+}
--- lc/src/LC/SOL/OptimalDivision.java
+++ lc/src/LC/SOL/OptimalDivision.java
@@ -3,8 +3,27 @@ package LC.SOL;
 public class OptimalDivision {
     class Solution {
         public String optimalDivision(int[] nums) {
+            StringBuilder b = new StringBuilder();
+            if (nums.length == 0) {
+                return b.toString();
+            }
+            if (nums.length == 1) {
+                b.append(nums[0]);
+                return b.toString();
+            }
+            if (nums.length == 2) {
+                b.append(nums[0]);
+                b.append("/");
+                b.append(nums[1]);
+                return b.toString();
+            }
 
-
+            b.append(nums[0]).append('/').append('(');
+            for (int i = 1; i < nums.length - 1; i++) {
+                b.append(nums[i]).append('/');
+            }
+            b.append(nums[nums.length - 1]).append(')');
+            return b.toString();
         }
     }
 }
