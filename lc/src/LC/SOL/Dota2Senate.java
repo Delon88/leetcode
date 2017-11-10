9d2429e52b26b547493114123e65814b906810f1
Nanhong Ye <nanhongy@amazon.com>
2017-11-09 16:59:37 -0800

    x

--- lc/src/LC/SOL/Dota2Senate.java
+++ lc/src/LC/SOL/Dota2Senate.java
@@ -1,8 +1,21 @@
 package LC.SOL;
 
+import java.util.TreeSet;
+
 public class Dota2Senate {
     class Solution {
         public String predictPartyVictory(String senate) {
+            TreeSet<Integer> r = new TreeSet<>();
+            TreeSet<Integer> d = new TreeSet<>();
+            for (int i = 0; i < senate.length(); i++) {
+                if (senate.charAt(i) == 'R') {
+                    r.add(i);
+                } else {
+                    d.add(i);
+                }
+            }
+
+
 
         }
     }
