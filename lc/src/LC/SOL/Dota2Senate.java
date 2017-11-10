516c0a069d30eeb436289be4af5fea46f8cec1e1
Delon88 <Mr.AlainDelon@gmail.com>
2017-11-09 23:17:47 -0800

    x

--- lc/src/LC/SOL/Dota2Senate.java
+++ lc/src/LC/SOL/Dota2Senate.java
@@ -1,22 +1,31 @@
 package LC.SOL;
 
-import java.util.TreeSet;
+import java.util.LinkedList;
 
 public class Dota2Senate {
     class Solution {
         public String predictPartyVictory(String senate) {
-            TreeSet<Integer> r = new TreeSet<>();
-            TreeSet<Integer> d = new TreeSet<>();
-            for (int i = 0; i < senate.length(); i++) {
-                if (senate.charAt(i) == 'R') {
+            int n = senate.length();
+            LinkedList<Integer> r = new LinkedList<>();
+            LinkedList<Integer> d = new LinkedList<>();
+            for ( int i = 0 ; i < senate.length() ; i++) {
+                if ( senate.charAt(i) == 'R') {
                     r.add(i);
                 } else {
                     d.add(i);
                 }
             }
 
+            while ( !r.isEmpty() && !d.isEmpty()) {
+                int rIn = r.pollFirst() , dIn = d.pollFirst();
+                if ( rIn < dIn ) {
+                    r.add( rIn + n);
+                } else {
+                    d.add(dIn + n);
+                }
+            }
 
-
+            return ( r.size() > d.size()) ? "Radiant" : "Dire";
         }
     }
 }
---
+++ lc/src/LC/SOL/_4KeysKeyboard.java
@@ -0,0 +1,13 @@
+package LC.SOL;
+
+public class _4KeysKeyboard {
+    class Solution {
+        public int maxA(int N) {
+            if ( N == 1) return 1;
+            int[] dp = new int[N + 1];
+            for ( int i = 2 ; i <= N ; i++) {
+                
+            }
+        }
+    }
+}
