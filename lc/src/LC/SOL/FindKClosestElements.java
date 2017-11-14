4833664ffda13b5fd7756fb653572edd602bda66
Nanhong Ye <nanhongy@amazon.com>
2017-11-13 16:02:44 -0800

    x.

--- lc/src/LC/SOL/FindKClosestElements.java
+++ lc/src/LC/SOL/FindKClosestElements.java
@@ -1,11 +1,25 @@
 package LC.SOL;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FindKClosestElements {
     class Solution {
         public List<Integer> findClosestElements(int[] arr, int k, int x) {
-
+            int start = 0, end = arr.length - k;
+            while (start < end) {
+                int mid = (start + end) / 2;
+                if (x - arr[mid] > arr[mid + k] - x) {
+                    start = mid + 1;
+                } else {
+                    end = mid;
+                }
+            }
+            List<Integer> ret = new ArrayList<>();
+            for (int i = start; i < start + k; i++) {
+                ret.add(arr[i]);
+            }
+            return ret;
         }
     }
 }
---
+++ lc/src/LC/SOL/SplitArrayintoConsecutiveSubsequences.java
@@ -0,0 +1,9 @@
+package LC.SOL;
+
+public class SplitArrayintoConsecutiveSubsequences {
+    class Solution {
+        public boolean isPossible(int[] nums) {
+
+        }
+    }
+}
