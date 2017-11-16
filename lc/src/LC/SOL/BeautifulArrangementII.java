e950ed3d7cf5f86937d44988116a083545575317
Nanhong <nanhongy@amazon.com>
2017-11-15 16:45:13 -0800

    x.

---
+++ lc/src/LC/SOL/BeautifulArrangementII.java
@@ -0,0 +1,16 @@
+package LC.SOL;
+
+public class BeautifulArrangementII {
+    class Solution {
+        public int[] constructArray(int n, int k) {
+            int[] ret = new int[n];
+            for (int i = 0; i < n; i++) {
+                ret[i] = i + 1;
+            }
+
+            for (int i = 0; i < k; k++) {
+                
+            }
+        }
+    }
+}
