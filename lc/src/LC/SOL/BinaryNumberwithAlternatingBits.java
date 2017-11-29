5477ceeda31e9c954f905774e9964b87e711f96c
Nanhong Ye <nanhongy@amazon.com>
2017-11-28 17:18:23 -0800

    x.

--- lc/src/LC/SOL/BinaryNumberwithAlternatingBits.java
+++ lc/src/LC/SOL/BinaryNumberwithAlternatingBits.java
@@ -1,4 +1,25 @@
 package LC.SOL;
 
 public class BinaryNumberwithAlternatingBits {
+    class Solution {
+
+        public boolean hasAlternatingBits(int n) {
+            n = n ^ (n >> 2);
+            return (n & (n - 1)) == 0;
+        }
+//        public boolean hasAlternatingBits(int n) {
+//            boolean al = false;
+//            if ((n & 1) == 1) al = true;
+//            while (n > 0) {
+//                if (al) {
+//                    if ((n & 1) == 0) return false;
+//                } else {
+//                    if ((n & 1) == 1) return false;
+//                }
+//                al = !al;
+//                n = n >>> 1;
+//            }
+//            return true;
+//        }
+    }
 }
