d4d665cab95af7d8a61a609fcde630de4c96895e
Nanhong Ye <nanhongy@amazon.com>
2018-03-09 17:48:54 -0800

    x.

--- lc/src/LC/SOL/SuperWashingMachines.java
+++ lc/src/LC/SOL/SuperWashingMachines.java
@@ -1,14 +1,30 @@
 package LC.SOL;
 
+import java.util.Arrays;
+
 public class SuperWashingMachines {
     class Solution {
         public int findMinMoves(int[] machines) {
-            int sum = 0;
-            for ( int m : machines) {
-                sum+= m;
+
+            long sum = 0;
+            for (int n : machines) {
+                sum += n;
             }
-            if ( sum % 3 != 0 ) return -1;
-            
+            if (sum % machines.length != 0) return -1;
+
+            int[] diff = new int[machines.length];
+
+            int target = (int) (sum / machines.length);
+            for (int i = 0; i < machines.length; i++) {
+                diff[i] = machines[i] - target;
+            }
+            System.out.println(Arrays.toString(diff));
+            int ret = 0;
+            for (int i = 0; i < machines.length - 1; i++) {
+                ret += Math.abs(diff[i]);
+                diff[i + 1] = diff[i] + diff[i + 1];
+            }
+            return ret;
         }
     }
 }
