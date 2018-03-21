105b3519ae8626b16bc918d7396004dff8be3ad5
Nanhong Ye <nanhongy@amazon.com>
2018-03-21 11:30:11 -0700

    x.

--- lc/src/LC/SOL/MaximumSumof3NonOverlappingSubarrays.java
+++ lc/src/LC/SOL/MaximumSumof3NonOverlappingSubarrays.java
@@ -5,18 +5,40 @@ public class MaximumSumof3NonOverlappingSubarrays {
         // dp
         public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
             int n = nums.length;
-            int[] left = new int[n - k + 1];
-            int[] right = new int[n  - k + 1];
-            int tmp = 0;
-            for ( int i = 0 ; i < n ; i++ ) {
-                tmp += nums[i];
-                if ( i < k  ) {
-                    continue;
+            if (n < 3 * k) return new int[0];
+            int[] sum = new int[n + 1];
+            for (int i = 0; i < n; i++) sum[i + 1] = sum[i] + nums[i];
+            int[] left = new int[n];
+            for (int i = k, tmp = sum[k] - sum[0]; i < n; i++) {
+                if (sum[i + 1] - sum[i + 1 - k] > tmp) {
+                    left[i] = i + 1 - k;
+                    tmp = sum[i + 1] - sum[i - k + 1];
                 } else {
-
-                    tmp -= nums[i - k];
+                    left[i] = left[i - 1];
                 }
             }
+            int[] right = new int[n];
+            right[n - k] = n - k;
+            for (int i = n - k - 1, tmp = sum[n] - sum[n - k]; i >= 0; i--) {
+                if (sum[i + k] - sum[i] > tmp) {
+                    right[i] = i;
+                    tmp = sum[i + k] - sum[i];
+                } else {
+                    right[i] = right[i + 1];
+                }
+            }
+
+            int max = Integer.MIN_VALUE;
+            int[] ret = {-1, -1, -1};
+            for (int i = k; i <= n - 2 * k; i++) {
+                int l = left[i - 1], r = right[i + k];
+                int tmp = sum[l + k] - sum[l] + sum[i + k] - sum[i] + sum[r + k] - sum[r];
+                if (tmp > max) {
+                    max = tmp;
+                    ret[0] = l ; ret[1] = i; ret[2] = r;
+                }
+            }
+            return ret;
         }
     }
 }
