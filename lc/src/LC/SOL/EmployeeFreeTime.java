9e5ce597dcfa0f64b3c2d2e65a585b1ff1065fb6
Nanhong Ye <nanhongy@amazon.com>
2018-03-28 13:46:52 -0700

    x.

---
+++ lc/src/LC/SOL/EmployeeFreeTime.java
@@ -0,0 +1,39 @@
+package LC.SOL;
+
+import LC.DS.Interval;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class EmployeeFreeTime {
+    class Solution {
+        //        Input: schedule = [[[1,2],[5,6]],[[1,3]],[[4,10]]]
+//        Output: [[3,4]]
+//        Explanation:
+//        There are a total of three employees, and all common
+//        free time intervals would be [-inf, 1], [3, 4], [10, inf].
+//        We discard any intervals that contain inf as they aren't finite.
+        public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
+            List<Interval> list = new ArrayList<>();
+            List<Interval> free = new ArrayList<>();
+            for (List<Interval> s : schedule) {
+                list.addAll(s);
+            }
+            Collections.sort(list, (a, b) -> a.start - b.start);
+            Interval tmp = list.get(0);
+            int min = tmp.start, max = tmp.end;
+            for (Interval in : list) {
+                if (in.start > tmp.end) {
+                    free.add(new Interval(tmp.end, in.start));
+                    tmp.start = in.start;
+                    tmp.end = in.end;
+                } else {
+                    tmp.end = Math.max(in.end , tmp.end);
+                }
+            }
+            return free;
+        }
+    }
+}
--- lc/src/LC/SOL/SetIntersectionSizeAtLeastTwo.java
+++ lc/src/LC/SOL/SetIntersectionSizeAtLeastTwo.java
@@ -1,10 +1,9 @@
 package LC.SOL;
 
-import java.util.Arrays;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class SetIntersectionSizeAtLeastTwo {
-//    An integer interval [a, b] (for integers a < b) is a set of all consecutive integers from a to b, including a and b.
+    //    An integer interval [a, b] (for integers a < b) is a set of all consecutive integers from a to b, including a and b.
 //
 //    Find the minimum size of a set S such that for every integer interval A in intervals, the intersection of S with A has size at least 2.
 //    Example 1:
@@ -15,18 +14,33 @@ public class SetIntersectionSizeAtLeastTwo {
 //    Consider the set S = {2, 3, 4}.  For each interval, there are at least 2 elements from S in the interval.
 //    Also, there isn't a smaller size set that fulfills the above condition.
 //    Thus, we output the size of this set, which is 3.
-
+//    [1, 3] [3 ,5]
     class Solution {
         public int intersectionSizeTwo(int[][] intervals) {
             Arrays.sort(intervals, (a, b) -> {
                 if (a[0] != b[0]) return a[0] - b[0];
-                else return a[1] - b[1];
+                else return b[1] - a[1];
             });
-            PriorityQueue<Integer> q = new PriorityQueue<>();
-            q.offer(intervals[0][1]);
-            for (int i = 1; i < intervals.length; i++) {
-
+            Stack<int[]> stack = new Stack<>();
+            for (int[] in : intervals) {
+                while (!stack.isEmpty() && stack.peek()[1] >= in[1]) stack.pop();
+                stack.push(in);
             }
+            int[][] ins = new int[stack.size()][2];
+            for (int i = stack.size() - 1; i >= 0; i--) {
+                ins[i] = stack.pop();
+            }
+            int ret = 2;
+            int p1 = ins[0][1] - 1, p2 = ins[0][1];
+            for (int i = 1; i < ins.length; i++) {
+                int overlap= 0 ;
+                if ( p1 >= ins[i][0] && p1 <= ins[i][1]) overlap++;
+                if ( p2 >= ins[i][0] && p2 <= ins[i][1]) overlap++;
+                if ( overlap >= 2) {continue;}
+                else if ( overlap == 1) { ret+=1; p1 = p2;p2 = ins[i][1];}
+                else if ( overlap == 0) { ret+=2; p1 = ins[i][1] - 1; p2 = ins[i][1];}
+            }
+            return ret;
         }
     }
 }
