261ebbd4eb2d19c6a0bf7cedf9ce38b5c4539e4f
U-DESKTOP-ASVFKQ3\nanhong <Mr.AlainDelon@gmail.com>
2018-03-28 23:15:55 -0700

    iii.

--- lc/src/LC/SOL/MinimizeMaxDistancetoGasStation.java
+++ lc/src/LC/SOL/MinimizeMaxDistancetoGasStation.java
@@ -1,9 +1,25 @@
 package LC.SOL;
 
+import java.util.Arrays;
+
 public class MinimizeMaxDistancetoGasStation {
     class Solution {
         public double minmaxGasDist(int[] stations, int K) {
-
+            int n = stations.length;
+            double low = 0, high = stations[n - 1] - stations[0], mid = 0.0d;
+            while (low + 1e-6 < high) {
+                mid = low + (high - low) / 2;
+                int count = 0;
+                for (int i = 0; i < n - 1; i++) {
+                    count += Math.ceil((stations[i + 1] - stations[i]) / mid) - 1;
+                }
+                if (count <= K) {
+                    high = mid;
+                } else {
+                    low = mid;
+                }
+            }
+            return low;
         }
     }
 }
---
+++ lc/src/LC/SOL/SwiminRisingWater.java
@@ -0,0 +1,9 @@
+package LC.SOL;
+
+public class SwiminRisingWater {
+    class Solution {
+        public int swimInWater(int[][] grid) {
+
+        }
+    }
+}
