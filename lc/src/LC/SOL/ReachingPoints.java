2dcc329e123a55b3b9d543ce010ef515d25650b7
U-DESKTOP-ASVFKQ3\nanhong <Mr.AlainDelon@gmail.com>
2018-03-29 23:13:56 -0700

    i.

---
+++ lc/src/LC/SOL/ReachingPoints.java
@@ -0,0 +1,19 @@
+package LC.SOL;
+
+public class ReachingPoints {
+    class Solution {
+        public boolean reachingPoints(int sx, int sy, int tx, int ty) {
+            // if ( tx > ty )  ->  ax + ay , ay    tx - ty , ty
+            while (tx >= sx && ty >= sy) {
+                if (tx > ty) {
+                    if (sy == ty) return (tx - sx) % ty == 0;
+                    tx = tx % ty;
+                } else {
+                    if (sx == tx ) return (ty - sy) % tx == 0;
+                    ty = ty % tx;
+                }
+            }
+            return false;
+        }
+    }
+}
