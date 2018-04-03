c2baa1fb07992a8a885c5fd373c0e89333a775ce
Nanhong Ye <nanhongy@amazon.com>
2018-04-03 15:11:15 -0700

    x.

--- lc/src/LC/SOL/LFUCache.java
+++ lc/src/LC/SOL/LFUCache.java
@@ -1,48 +1,100 @@
 package LC.SOL;
 
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.Map;
 
 public class LFUCache {
+//
+//    Design and implement a data structure for Least Frequently Used (LFU) cache. It should support the following operations: get and put.
+//
+//    get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+//    put(key, value) - Set or insert the value if the key is not already present. When the cache reaches its capacity,
+//    it should invalidate the least frequently used item before inserting a new item.
+//    For the purpose of this problem, when there is a tie (i.e., two or more keys that have the same frequency),
+//    the least recently used key would be evicted.
+//
+//    Follow up:
+//    Could you do both operations in O(1) time complexity?
+//
+//    Example:
+//
+//    LFUCache cache = new LFUCache( 2 /* capacity */ );
 
-    class DoubleLinkedListNode {
-        DoubleLinkedListNode prev;
-        DoubleLinkedListNode next;
-        int key;
-        int val;
-        int freq = 1;
-    }
-    private int CAPA ;
-    private DoubleLinkedListNode head;
-    Map<Integer,DoubleLinkedListNode> map;
+//cache.put(1, 1);
+//cache.put(2, 2);
+//cache.get(1);       // returns 1
+//cache.put(3, 3);    // evicts key 2
+//cache.get(2);       // returns -1 (not found)
+//cache.get(3);       // returns 3.
+//cache.put(4, 4);    // evicts key 1.
+//cache.get(1);       // returns -1 (not found)
+//cache.get(3);       // returns 3
+//cache.get(4);       // returns 4
+
+    // key value
+    Map<Integer, Integer> cache;
+    // key freq
+    Map<Integer, Integer> freqMap;
+    // freq -> key set
+    Map<Integer, LinkedHashSet<Integer>> freqToKey;
+
+    private int CAPA;
+
+    int minFreq = -1;
+
     public LFUCache(int capacity) {
+        cache = new HashMap<>();
+        freqMap = new HashMap<>();
+        freqToKey = new HashMap<>();
         CAPA = capacity;
-        DoubleLinkedListNode head = new DoubleLinkedListNode();
-        head.next = head;
-        head.prev = head;
-        head.freq = -1;
-        map = new HashMap<>();
     }
 
     public int get(int key) {
-        if ( map.containsKey(key)) {
-            DoubleLinkedListNode node = map.get(key);
-            // update freq and node pos
-            if ( node.prev == head ) {
-                node.freq++;
-                return node.val;
-            } else {
-                if ( node.prev.val > node.val + 1 ) {
-                    node.freq++;
-                    return node.val;
+        if ( cache.containsKey(key)) {
+            int value = cache.get(key);
+            int fr = freqMap.get(key);
+            freqToKey.get(fr).remove(key);
+            if (freqToKey.get(fr).size() == 0) {
+                freqToKey.remove(fr);
+                if ( fr == minFreq) {
+                    minFreq++;
                 }
             }
+
+            fr++;
+            freqMap.put(key, fr);
+            if ( !freqToKey.containsKey(fr)) {
+                freqToKey.put(fr, new LinkedHashSet<>());
+            }
+            freqToKey.get(fr).add(key);
+            return value;
         } else {
             return -1;
         }
     }
 
     public void put(int key, int value) {
-
+        if ( CAPA <= 0) return;
+        if ( cache.containsKey(key)) {
+            get(key);
+            cache.put(key, value);
+        } else {
+            if (cache.size() >= CAPA) {
+                // evict oldest
+                int evict = freqToKey.get(minFreq).iterator().next();
+                freqToKey.get(minFreq).remove(evict);
+                if (freqToKey.get(minFreq).size() == 0) freqToKey.remove(minFreq);
+                cache.remove(evict);
+                freqMap.remove(evict);
+            }
+            if (!freqToKey.containsKey(1)) {
+                freqToKey.put(1, new LinkedHashSet<>());
+            }
+            minFreq = 1;
+            freqToKey.get(1).add(key);
+            cache.put(key, value);
+            freqMap.put(key, 1);
+        }
     }
 }
--- lc/src/LC/SOL/SmallestGoodBase.java
+++ lc/src/LC/SOL/SmallestGoodBase.java
@@ -1,9 +1,57 @@
 package LC.SOL;
 
+import java.math.BigInteger;
+
 public class SmallestGoodBase {
+    //
+//    For an integer n, we call k>=2 a good base of n, if all digits of n base k are 1.
+//
+//    Now given a string representing n, you should return the smallest good base of n in string format.
+//
+//            Example 1:
+//
+//    Input: "13"
+//    Output: "3"
+//    Explanation: 13 base 3 is 111.
+//
+//    Example 2:
+//
+//    Input: "4681"
+//    Output: "8"
+//    Explanation: 4681 base 8 is 11111.
+//
+//    Example 3:
+//
+//    Input: "1000000000000000000"
+//    Output: "999999999999999999"
+//    Explanation: 1000000000000000000 base 999999999999999999 is 11.
+//
+//    Note:
+//
+//    The range of n is [3, 10^18].
+//    The string representing n is always valid and will not have leading zeros.
+//   n =  1 + 1 * k ^ 1 + 1 * k ^ 2 ... 1 * k ^ m
+//   n - 1 =  1 * k ^ 1 + 1 * k ^ 2 ... 1 * k ^ m
+//   n -1 = k ( 1 + k^ 1 + ... k^ m -1)
+//   n - k ^ m = 1 + ... k ^m - 1
+//   (n -1)/ k = n - k^m
+//   n - 1 = kn - k ^ m + 1
+//   k^m +1  - 1 = n( k - 1);
+//   k^(m+1) -1 / k - 1 = n;  1)
+//    n > k^m 1)
+//    n < ( k + 1) ^ m
+//    m th square root of n > k
+//    k + 1 > m th square root of n > k
     class Solution {
         public String smallestGoodBase(String n) {
-            return null;
+            Long num = Long.parseLong(n);
+            BigInteger bn = BigInteger.valueOf(num);
+            int maxBase = (int) (Math.log(num) / (long) Math.log(2));
+            for (int b =maxBase; b >= 2; b--) {
+                BigInteger k = BigInteger.valueOf((long) Math.pow(num , 1.0d/ b ));
+                BigInteger left = k.pow(b + 1).subtract(BigInteger.ONE);
+                BigInteger right = bn.multiply(k.subtract(BigInteger.ONE));
+            }
         }
     }
 }
