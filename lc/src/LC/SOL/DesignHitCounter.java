a1a105b9a1c191e2a74bf018570c36f058162c23
Delon88 <Mr.AlainDelon@gmail.com>
2018-05-31 09:31:39 -0700

    x.

---
+++ lc/src/LC/SOL/DesignHitCounter.java
@@ -0,0 +1,23 @@
+package LC.SOL;
+
+public class DesignHitCounter {
+    class HitCounter {
+
+        /** Initialize your data structure here. */
+        public HitCounter() {
+
+        }
+
+        /** Record a hit.
+         @param timestamp - The current timestamp (in seconds granularity). */
+        public void hit(int timestamp) {
+
+        }
+
+        /** Return the number of hits in the past 5 minutes.
+         @param timestamp - The current timestamp (in seconds granularity). */
+        public int getHits(int timestamp) {
+
+        }
+    }
+}
