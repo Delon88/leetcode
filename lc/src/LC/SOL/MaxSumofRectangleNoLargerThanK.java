b43e96bfc4a8062c1daf4b506bb5c0e91c1de007
Nanhong Ye <nanhongy@amazon.com>
2018-05-31 14:11:41 -0700

    x.

---
+++ lc/src/LC/SOL/MaxSumofRectangleNoLargerThanK.java
@@ -0,0 +1,20 @@
+package LC.SOL;
+
+import java.util.TreeSet;
+
+
+public class MaxSumofRectangleNoLargerThanK {
+
+
+//    Given matrix = [
+//            [1,  0, 1],
+//            [0, -2, 3]
+//            ]
+//    k = 2
+
+    class Solution {
+        public int maxSumSubmatrix(int[][] matrix, int target) {
+            
+        }
+    }
+}
