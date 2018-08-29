916914fba67ab68e94ed23f53f9113ac4c47a9bd
Delon88 <Mr.AlainDelon@gmail.com>
2018-08-29 10:10:36 -0700

    x.

---
+++ lc/src/LC/SOL/New21Game.java
@@ -0,0 +1,14 @@
+package LC.SOL;
+
+public class New21Game {
+    class Solution {
+        public double new21Game(int N, int K, int W) {
+            if (K == 0 || N >= K + W) return 1;
+            int[] dp = new int[N + 1];
+            dp[0] =1;
+            for (int i = 1; i <= N; i++) {
+                dp[i] =
+            }
+        }
+    }
+}
