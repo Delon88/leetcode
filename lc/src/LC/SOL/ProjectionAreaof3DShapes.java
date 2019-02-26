ed5b14a90057a3aeff2530755227dc1ba013c9b2
Nanhong Ye <nanhongy@amazon.com>
2019-02-25 19:34:42 -0800

    x.

--- lc/src/LC/SOL/ProjectionAreaof3DShapes.java
+++ lc/src/LC/SOL/ProjectionAreaof3DShapes.java
@@ -4,7 +4,17 @@ public class ProjectionAreaof3DShapes {
     class Solution {
         public int projectionArea(int[][] grid) {
             int ret = 0,  n = grid.length;
-
+            int x  = 0, y = 0 , z = 0;
+            for ( int i = 0 ; i < grid.length ; i++) {
+                int tx = 0 , ty = 0;
+                for ( int j = 0 ; j < grid[0].length ; j++) {
+                    tx = Math.max( tx , grid[j][i]);
+                    ty = Math.max( ty , grid[i][j]);
+                    if ( grid[i][j] > 0 )  z++;
+                }
+                x += tx ; y += ty;
+            }
+            return x + y + z;
         }
     }
 }
