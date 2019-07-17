5f81fb917e54f02c8fbea5f80b14871806c532f4
U-DESKTOP-I7TIMET\Nanhong Ye <Nanhong.Ye@gmail.com>
2019-07-16 22:10:10 -0700

    x.

--- lc/src/LC/SOL/NumbersAtMostNGivenDigitSet.java
+++ lc/src/LC/SOL/NumbersAtMostNGivenDigitSet.java
@@ -2,14 +2,27 @@ package LC.SOL;
 
 public class NumbersAtMostNGivenDigitSet {
     class Solution {
+        // dp[i] = Math.max(
         public int atMostNGivenDigitSet(String[] D, int N) {
             String S = Integer.toString(N);
-            int len = S.length();
-            int[] dp = new int[len + 1];
-            dp[len] = 1;
-            for (int i = len - 1; i >= 0; i--) {
-                
+            int K = S.length();
+            int[] dp = new int[K + 1];
+            dp[K] = 1;
+            for (int i = K - 1; i >= 0; i--) {
+                int curD = S.charAt(i) - '0';
+                for (String d : D) {
+                    if ( curD > Integer.valueOf(d)) {
+                        dp[i] += Math.pow(D.length, K - 1 - i);
+                    } else if ( curD == Integer.valueOf(d)) {
+                        dp[i] += dp[i + 1];
+                    }
+                }
             }
+            int ret = dp[0];
+            for ( int i = 1 ; i < K ; i++) {
+                ret += Math.pow(D.length, i);
+            }
+            return ret;
         }
     }
 }
