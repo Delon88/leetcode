938d8edd07a5809a5c36295ebe4899ffb3fd9213
Delon88 <Mr.AlainDelon@gmail.com>
2020-08-02 00:06:04 -0700

    x.

---
+++ lc/src/LC/SOL/ReverseSubarrayToMaximizeArrayValue.java
@@ -0,0 +1,9 @@
+package LC.SOL;
+
+public class ReverseSubarrayToMaximizeArrayValue {
+    static class Solution {
+        public int maxValueAfterReverse(int[] nums) {
+            
+        }
+    }
+}
--- lc/src/LC/SOL/SorttheMatrixDiagonally.java
+++ lc/src/LC/SOL/SorttheMatrixDiagonally.java
@@ -1,9 +1,26 @@
 package LC.SOL;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 public class SorttheMatrixDiagonally {
     static class Solution {
         public int[][] diagonalSort(int[][] mat) {
-
+            int m = mat.length, n = mat[0].length;
+            Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    map.putIfAbsent(i - j, new PriorityQueue<>());
+                    map.get(i - j).offer(mat[i][j]);
+                }
+            }
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    mat[i][j] = map.get(i - j).poll();
+                }
+            }
+            return mat;
         }
     }
 }
