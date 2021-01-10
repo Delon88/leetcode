9e911f3ad8a780c7dc8a2656880584280ad37316
Nanhong Ye <Nanhong.Ye@gmail.com>
2021-01-10 00:55:33 -0800

    x.

--- lc/src/LC/SOL/FilterRestaurantsbyVeganFriendlyPriceandDistance.java
+++ lc/src/LC/SOL/FilterRestaurantsbyVeganFriendlyPriceandDistance.java
@@ -1,5 +1,9 @@
 package LC.SOL;
 
 public class FilterRestaurantsbyVeganFriendlyPriceandDistance {
-    
+    class Solution {
+        public List<Integer> filterRestaurants(int[][] restaurants, int veganFriendly, int maxPrice, int maxDistance) {
+            //TODO
+        }
+    }
 }
