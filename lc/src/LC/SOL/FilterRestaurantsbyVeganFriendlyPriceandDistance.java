697fbe8c5a5677dc175215cebbbb9448c8bec6a8
Nanhong Ye <Nanhong.Ye@gmail.com>
2021-01-31 01:23:54 -0800

    x.

--- lc/src/LC/SOL/FilterRestaurantsbyVeganFriendlyPriceandDistance.java
+++ lc/src/LC/SOL/FilterRestaurantsbyVeganFriendlyPriceandDistance.java
@@ -1,9 +1,19 @@
 package LC.SOL;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FilterRestaurantsbyVeganFriendlyPriceandDistance {
     class Solution {
         public List<Integer> filterRestaurants(int[][] restaurants, int veganFriendly, int maxPrice, int maxDistance) {
-            //TODO
+            return Arrays.stream(restaurants)
+                    .filter(r -> r[2] >= veganFriendly && r[3] <= maxPrice && r[4] <= maxDistance).sorted((a, b) -> {
+                        if (a[1] == b[1])
+                            return b[0] - a[0];
+                        else
+                            return b[1] - a[1];
+                    }).map(r -> r[0]).collect(Collectors.toList());
         }
     }
 }
---
+++ lc/src/LC/SOL/FindtheCityWiththeSmallestNumberofNeighborsataThresholdDistance.java
@@ -0,0 +1,29 @@
+package LC.SOL;
+
+import java.util.Arrays;
+
+public class FindtheCityWiththeSmallestNumberofNeighborsataThresholdDistance {
+    class Solution {
+        public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+            int[][] dist = new int[n][n];
+            int res = 0, small = n;
+            for (int[] r : dist) {
+                Arrays.fill(r, 10001);
+            }
+            for (int[] e : edges) {
+                dist[e[0]][e[1]] = dist[e[1]][e[0]] = e[2];
+            }
+            for (int i = 0; i < n; i++)
+                dist[i][i] = 0;
+            for (int k = 0; k < n; k++) {
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+                        if ( dist[i][j] > dist[i][k] + dist[k][j]) {
+                            dist[i][j] = dist[i][k] + dist[k][j];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
