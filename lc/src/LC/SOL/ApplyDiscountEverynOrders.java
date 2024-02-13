4d747108aa33d138365331e9c0d8dff5b93edb93
Delon88 <Mr.AlainDelon@gmail.com>
2024-02-12 22:45:45 -0800

    xx

---
+++ lc/src/LC/SOL/ApplyDiscountEverynOrders.java
@@ -0,0 +1,29 @@
+package LC.SOL;
+
+public class ApplyDiscountEverynOrders {
+    class Cashier {
+
+        int discount;
+        int n;
+        int[] products;
+        int[] prices;
+        int customer = 0;
+        public Cashier(int n, int discount, int[] products, int[] prices) {
+            this.n = n;
+            this.discount = discount;
+            this.product = products;
+            this.prices = prices;
+        }
+        
+        public double getBill(int[] product, int[] amount) {
+            
+        }
+    }
+    
+    /**
+     * Your Cashier object will be instantiated and called as such:
+     * Cashier obj = new Cashier(n, discount, products, prices);
+     * double param_1 = obj.getBill(product,amount);
+     */
+    
+}
